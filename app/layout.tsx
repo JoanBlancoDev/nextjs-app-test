8a11f850d0140d969537c12a2b1be04cc587c732
Joan Blanco <joanblanco.dev@gmail.com>
2024-07-30 17:50:12 -0400

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css' // Import global styles located in UI folder
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={ `${montserrat.className} antialiased` }>
+      {children}
+      <footer className='py-10 flex justify-center items-center'>Hecho por amor por la gente de vercel</footer>
+      </body>
     </html>
   );
 }
